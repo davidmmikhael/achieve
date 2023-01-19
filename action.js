053ede111219a1230c437a5a8ac19e6d7cfbfdfa
David Mikhael <davidmmikhael@icloud.com>
2023-01-19 06:42:14 -0800

    Start of Portfolio Site: added 3 diff files plus the README. There's a note and a paper wireframe as well.

---
+++ action.js
@@ -0,0 +1,17 @@
+let animation = document.getElementById('joy');
+
+const fun = () => {
+
+	animation.style.fontSize = '100px';
+
+
+};
+
+/*const laugh = () => {
+	animation.style.fontSize -= '1px';
+};*/
+
+
+	
+animation.onclick = fun;
+//animation.addEventListener('onkeydown', laugh());
